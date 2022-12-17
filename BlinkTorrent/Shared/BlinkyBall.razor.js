42aa5bda3eed19c24683c143be510e77a38b6870
jpmikkers <jean.paul.mikkers@gmail.com>
2022-12-17 22:44:11 +0100

    simplify BlinkyAnimator constructor

--- BlinkTorrent/Shared/BlinkyBall.razor.js
+++ BlinkTorrent/Shared/BlinkyBall.razor.js
@@ -1,21 +1,27 @@
 ﻿
 class BlinkyAnimator {
-    constructor(eyesOpenElt, eyesClosedElt, leftPupilElt, rightPupilElt) {
-        this.eyesOpen = eyesOpenElt
-        this.eyesClosed = eyesClosedElt
-        this.leftPupil = leftPupilElt
-        this.rightPupil = rightPupilElt
-        this.eyesAreOpen = false
-        this.lpcx = Number(this.leftPupil.getAttributeNS(null, 'cx'))
-        this.lpcy = Number(this.leftPupil.getAttributeNS(null, 'cy'))
-        this.rpcx = Number(this.rightPupil.getAttributeNS(null, 'cx'))
-        this.rpcy = Number(this.rightPupil.getAttributeNS(null, 'cy'))
-        this.timer = -1
+    constructor(blinkyGroupElt) {
+        this.leftEyeOpen = blinkyGroupElt.querySelector(".blinkyLeftEyeOpen");
+        this.rightEyeOpen = blinkyGroupElt.querySelector(".blinkyRightEyeOpen");
+
+        this.leftEyeClosed = blinkyGroupElt.querySelector(".blinkyLeftEyeClosed");
+        this.rightEyeClosed = blinkyGroupElt.querySelector(".blinkyRightEyeClosed");
+
+        this.leftPupil = this.leftEyeOpen.querySelector(".blinkyPupil");
+        this.rightPupil = this.rightEyeOpen.querySelector(".blinkyPupil");
+
+        this.eyesAreOpen = false;
+
+        this.lpcx = Number(this.leftPupil.getAttributeNS(null, 'cx'));
+        this.lpcy = Number(this.leftPupil.getAttributeNS(null, 'cy'));
+        this.rpcx = Number(this.rightPupil.getAttributeNS(null, 'cx'));
+        this.rpcy = Number(this.rightPupil.getAttributeNS(null, 'cy'));
+        this.timer = -1;
         this.#startAnimation();
     }
 
     #startAnimation() {
-        this.#closeEyes();
+        this.#updateEyes();
         this.#animateBlinky();
     }
 
@@ -33,47 +39,57 @@ class BlinkyAnimator {
         let nextTimeout = 200;
 
         if (this.#getRandom(1, 100) < 50) {
-            this.#movePupils()
+            this.#movePupils();
         }
 
         if (this.eyesAreOpen) {
             if (this.#getRandom(1, 100) < 20) {
                 this.eyesAreOpen = false;
-                this.#closeEyes()
+                this.#updateEyes();
                 nextTimeout = 200;
             } else {
                 nextTimeout = this.#getRandom(500, 1000);
             }
         } else {
-            this.eyesAreOpen = true
-            this.#openEyes()
+            this.eyesAreOpen = true;
+            this.#updateEyes();
             nextTimeout = this.#getRandom(500, 1000);
         }
 
         this.timer = setTimeout(() => { this.#animateBlinky() }, nextTimeout);
     }
 
-    #closeEyes() {
-        this.eyesOpen.setAttributeNS(null, 'visibility', 'hidden')
-        this.eyesClosed.setAttributeNS(null, 'visibility', 'visible')
-    }
+    #updateEyes() {
+        if (this.eyesAreOpen) {
+            this.leftEyeOpen.setAttributeNS(null, 'visibility', 'visible');
+            this.leftEyeClosed.setAttributeNS(null, 'visibility', 'hidden');
+        }
+        else {
+            this.leftEyeOpen.setAttributeNS(null, 'visibility', 'hidden');
+            this.leftEyeClosed.setAttributeNS(null, 'visibility', 'visible');
+        }
 
-    #openEyes() {
-        this.eyesOpen.setAttributeNS(null, 'visibility', 'visible')
-        this.eyesClosed.setAttributeNS(null, 'visibility', 'hidden')
+        if (this.eyesAreOpen) {
+            this.rightEyeOpen.setAttributeNS(null, 'visibility', 'visible');
+            this.rightEyeClosed.setAttributeNS(null, 'visibility', 'hidden');
+        }
+        else {
+            this.rightEyeOpen.setAttributeNS(null, 'visibility', 'hidden');
+            this.rightEyeClosed.setAttributeNS(null, 'visibility', 'visible');
+        }
     }
 
     #movePupils() {
-        let dx = this.#getRandom(-4, 4)
-        let dy = this.#getRandom(-4, 4)
-        
-        this.leftPupil.setAttributeNS(null, 'cx', this.lpcx + dx)
-        this.leftPupil.setAttributeNS(null, 'cy', this.lpcy + dy)
-        this.rightPupil.setAttributeNS(null, 'cx', this.rpcx + dx)
-        this.rightPupil.setAttributeNS(null, 'cy', this.rpcy + dy)
+        let dx = this.#getRandom(-4, 4);
+        let dy = this.#getRandom(-4, 4);
+
+        this.leftPupil.setAttributeNS(null, 'cx', this.lpcx + dx);
+        this.leftPupil.setAttributeNS(null, 'cy', this.lpcy + dy);
+        this.rightPupil.setAttributeNS(null, 'cx', this.rpcx + dx);
+        this.rightPupil.setAttributeNS(null, 'cy', this.rpcy + dy);
     }
 }
 
-export function createAnimator(eyesOpenElt, eyesClosedElt, leftPupilElt, rightPupilElt) {
-    return new BlinkyAnimator(eyesOpenElt, eyesClosedElt, leftPupilElt, rightPupilElt)
+export function createAnimator(blinkyGroupElt) {
+    return new BlinkyAnimator(blinkyGroupElt);
 }
